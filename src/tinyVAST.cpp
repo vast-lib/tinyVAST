6400826573cb0182a0fad27b7e99a49741d4d2b6
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2023-10-23 10:59:41 -0700

    clean up variable and index naming conventions
    Former-commit-id: 52e394a8eb098698f1c2b4848491f9ef8f93b688 [formerly 12a59aebc370bee9aac0223372bcc5cdce59cf2e]
    Former-commit-id: 6df9821f0ab4e93fa59756dcc31c13e4e710019c

--- src/tinyVAST.cpp
+++ src/tinyVAST.cpp
@@ -7,75 +7,81 @@ bool isNA(Type x){
   return R_IsNA(asDouble(x));
 }
 
+//
+template<class Type>
+vector<Type> multiply_3d_sparse( matrix<int> A, vector<Type> weight, array<Type> x, int n_i ){
+  vector<Type> out( n_i );
+  out.setZero();
+  for( int z=0; z<A.rows(); z++ ){
+    if( (!isNA(A(z,0))) & (!isNA(A(z,1))) & (!isNA(A(z,2))) & (!isNA(A(z,3))) ){   // Ignoring epsilon_stc when `sem` not passed to fit(.)
+      out(A(z,0)) += weight(z) * x(A(z,1),A(z,2),A(z,3));
+    }
+  }
+  return out;
+}
+
 template<class Type>
 Type objective_function<Type>::operator() (){
-  //using namespace R_inla;
+  //using namespace R_inla;  // Not loaded globally, but used below
   using namespace density;
   using namespace Eigen;
 
   // Data
-  DATA_VECTOR(Y);        // The response
-  DATA_MATRIX(X);        // Design matrix for fixed covariates
-  DATA_MATRIX(Z);        // Design matrix for splines
+  DATA_VECTOR( y_i );        // The response
+  DATA_MATRIX( X_ij );        // Design matrix for fixed covariates
+  DATA_MATRIX( Z_ik );        // Design matrix for splines
   DATA_IVECTOR( t_i );
   DATA_IVECTOR( c_i );
-  DATA_SPARSE_MATRIX(S); // Sparse penalization matrix
-  DATA_IVECTOR(Sdims);   // Dimensions of blockwise components of S
+  DATA_SPARSE_MATRIX( S ); // Sparse penalization matrix
+  DATA_IVECTOR( Sdims );   // Dimensions of blockwise components of S
 
   // Spatial objects
   DATA_INTEGER( spatial_method_code );
-  DATA_IMATRIX(Aistc);
-  DATA_VECTOR(Ax);
+  DATA_IMATRIX( Aistc_zz );
+  DATA_VECTOR( Axi_z );
 
   // SEM objects
   DATA_IMATRIX( RAM );
   DATA_VECTOR( RAMstart );
 
   // Prediction options
-  DATA_MATRIX(predX);       // Design matrix for fixed covariates
-  //DATA_UPDATE(Xpred);       //
-  DATA_MATRIX(predZ);       // Design matrix for splines
-  //DATA_UPDATE(Zpred);       //
-  DATA_IMATRIX(predAistc);       // Design matrix for SPDE projection (must be dense for DATA_UPDATE)
-  DATA_VECTOR( predAx );
-  //DATA_UPDATE(Apred);       //
-  DATA_IVECTOR( predt );
-  //DATA_UPDATE( tpred );
-  DATA_IVECTOR( predc );
-  //DATA_UPDATE( cpred );
+  DATA_MATRIX( X_gj );       // Design matrix for fixed covariates
+  DATA_MATRIX( Z_gk );       // Design matrix for splines
+  DATA_IMATRIX( Agstc_zz );       // Design matrix for SPDE projection (must be dense for DATA_UPDATE)
+  DATA_VECTOR( Axg_z );
+  DATA_IVECTOR( t_g );
+  DATA_IVECTOR( c_g );
   //DATA_VECTOR( expansion );
 
   // Params
   PARAMETER(log_kappa);
-  //PARAMETER(log_tau);
-  PARAMETER_VECTOR(alpha); // Fixed covariate parameters
-  PARAMETER_VECTOR(gamma); // Spline regression parameters
+  PARAMETER_VECTOR( alpha_j ); // Fixed covariate parameters
+  PARAMETER_VECTOR( gamma_k ); // Spline regression parameters
   //PARAMETER_VECTOR(omega); // SPDE random effecs
-  PARAMETER_VECTOR(beta_z); // SEM coefficients
-  PARAMETER_VECTOR(log_lambda); //Penalization parameters
-  PARAMETER(log_sigma);
+  PARAMETER_VECTOR( beta_z ); // SEM coefficients
+  PARAMETER_VECTOR( log_lambda ); //Penalization parameters
+  PARAMETER_VECTOR( log_sigma );
   PARAMETER_VECTOR( delta0_c );
-  //PARAMETER_ARRAY( x_tc );
   PARAMETER_ARRAY( epsilon_stc );
 
   Type nll = 0;
 
   // Assemble precision
-  int n_c = epsilon_stc.cols();
-  int n_t = epsilon_stc.col(0).cols();
-  int n_s = epsilon_stc.rows();
-  int n_k = n_t * n_c;      // data
-  int k;
-  Eigen::SparseMatrix<Type> Q_kk( n_k, n_k );
+  int n_s = epsilon_stc.dim(0);
+  int n_t = epsilon_stc.dim(1);
+  int n_c = epsilon_stc.dim(2);
+  int n_h = n_t * n_c;      // data
+  Eigen::SparseMatrix<Type> Q_hh( n_h, n_h );
   // SEM
-  Eigen::SparseMatrix<Type> Linv_kk(n_k, n_k);
-  Eigen::SparseMatrix<Type> Rho_kk(n_k, n_k);
-  Eigen::SparseMatrix<Type> Gammainv_kk(n_k, n_k);
-  Eigen::SparseMatrix<Type> I_kk( n_k, n_k );
-  Rho_kk.setZero();
-  Gammainv_kk.setZero();
-  I_kk.setIdentity();
+  Eigen::SparseMatrix<Type> Linv_hh(n_h, n_h);
+  Eigen::SparseMatrix<Type> Rho_hh(n_h, n_h);
+  Eigen::SparseMatrix<Type> Gammainv_hh(n_h, n_h);
+  Eigen::SparseMatrix<Type> I_hh( n_h, n_h );
+  Rho_hh.setZero();
+  Gammainv_hh.setZero();
+  I_hh.setIdentity();
   Type tmp;
+  REPORT( n_h );
   for(int r=0; r<RAM.rows(); r++){
     // Extract estimated or fixed value
     if(RAM(r,3)>=1){
@@ -83,123 +89,115 @@ Type objective_function<Type>::operator() (){
     }else{
       tmp = RAMstart(r);
     }
-    if(RAM(r,0)==1) Rho_kk.coeffRef( RAM(r,1)-1, RAM(r,2)-1 ) = tmp;
-    if(RAM(r,0)==2) Gammainv_kk.coeffRef( RAM(r,1)-1, RAM(r,2)-1 ) = 1 / tmp;
+    if(RAM(r,0)==1) Rho_hh.coeffRef( RAM(r,1)-1, RAM(r,2)-1 ) = tmp;
+    if(RAM(r,0)==2) Gammainv_hh.coeffRef( RAM(r,1)-1, RAM(r,2)-1 ) = 1 / tmp;
   }
-  Linv_kk = Gammainv_kk * ( I_kk - Rho_kk );
-  Q_kk = Linv_kk.transpose() * Linv_kk;
+  Linv_hh = Gammainv_hh * ( I_hh - Rho_hh );
+  Q_hh = Linv_hh.transpose() * Linv_hh;
 
   // Calculate effect of initial condition -- SPARSE version
-  vector<Type> delta_k( n_k );
-  delta_k.setZero();
+  vector<Type> delta_h( n_h );
+  delta_h.setZero();
+  int h;
   if( delta0_c.size() > 0 ){
     // Compute delta_k
-    matrix<Type> delta0_k1( n_k, 1 );
-    delta0_k1.setZero();
+    matrix<Type> delta0_h1( n_h, 1 );
+    delta0_h1.setZero();
     for(int c=0; c<n_c; c++){
-      k = c * n_t;
-      delta0_k1(k,0) = delta0_c(c);
+      h = c * n_t;
+      delta0_h1(h,0) = delta0_c(c);
     }
 
-    // SPARSE version
-    // See C:\Users\James.Thorson\Desktop\Work files\AFSC\2023-06 -- Sparse inverse-product\Kasper example\lu.cpp
-    Eigen::SparseMatrix<Type> IminusRho_kk = I_kk - Rho_kk;
+    // Sparse product
+    Eigen::SparseMatrix<Type> IminusRho_hh = I_hh - Rho_hh;
     Eigen::SparseLU< Eigen::SparseMatrix<Type>, Eigen::COLAMDOrdering<int> > lu;
-    lu.compute(IminusRho_kk);
-    matrix<Type> x = lu.solve(delta0_k1);
+    lu.compute(IminusRho_hh);
+    matrix<Type> x = lu.solve(delta0_h1);
 
-    REPORT( delta0_k1 );
-    delta_k = x.array();
+    // Resize
+    delta_h = delta0_h1.array();
+    REPORT( delta_h );
   }
-  REPORT( delta_k );
 
   // Transformations
-  Type sigma = exp(log_sigma);
   vector<Type> lambda = exp(log_lambda);
-  array<Type> epsilon_sk( n_s, n_k );
+  array<Type> epsilon_sh( n_s, n_h );
   for( int s=0; s<n_s; s++ ){
   for( int t=0; t<n_t; t++ ){
   for( int c=0; c<n_c; c++ ){
-    k = c*n_t + t;
-    epsilon_sk(s,k) = epsilon_stc(s,t,c);
+    h = c*n_t + t;
+    epsilon_sh(s,h) = epsilon_stc(s,t,c);
   }}}
 
   // Spatial distribution
   Type log_tau = 0;
-  Eigen::SparseMatrix<Type> Q_spatial;
+  Eigen::SparseMatrix<Type> Q_ss;
   if( (spatial_method_code==1) | (spatial_method_code==3) | (spatial_method_code==4) ){
-    log_tau = log( 1.0 / (exp(log_kappa) * sqrt(4.0*M_PI)) );
+    // Using INLA
     DATA_STRUCT(spatial_list, R_inla::spde_t);
-    Q_spatial = R_inla::Q_spde(spatial_list, exp(log_kappa));
+    Q_ss = R_inla::Q_spde(spatial_list, exp(log_kappa));
+    log_tau = log( 1.0 / (exp(log_kappa) * sqrt(4.0*M_PI)) );
   }else if( spatial_method_code==2 ){
-    log_tau = 0.0;
+    /// Using SAR
     DATA_SPARSE_MATRIX( Adj );
     Eigen::SparseMatrix<Type> I_ss( Adj.rows(), Adj.rows() );
     Eigen::SparseMatrix<Type> Lspatial_ss( Adj.rows(), Adj.rows() );
     I_ss.setIdentity();
     Lspatial_ss = ( I_ss - exp(log_kappa)*Adj );
-    Q_spatial = Lspatial_ss.transpose() * Lspatial_ss;
+    Q_ss = Lspatial_ss.transpose() * Lspatial_ss;
+    log_tau = 0.0;
   }
 
   // distributions
-  if( (n_s * n_k) > 0 ){
-    nll += SEPARABLE( GMRF(Q_kk), GMRF(Q_spatial) )( epsilon_sk );
+  if( (n_s>0) & (n_h>0) ){
+    nll += SEPARABLE( GMRF(Q_hh), GMRF(Q_ss) )( epsilon_sh );
   }
 
   // Likelihood of spline components
-  k = 0;   // Counter
-  for(int i=0; i<Sdims.size(); i++){
-    int m_i = Sdims(i);
-    vector<Type> gamma_i = gamma.segment(k,m_i);       // Recover gamma_i
-    SparseMatrix<Type> S_i = S.block(k,k,m_i,m_i);  // Recover S_i
-    nll -= Type(0.5)*m_i*log_lambda(i) - 0.5*lambda(i)*GMRF(S_i).Quadform(gamma_i);
-    k += m_i;
+  int k = 0;   // Counter
+  for(int z=0; z<Sdims.size(); z++){
+    int m_z = Sdims(z);
+    vector<Type> gamma_segment = gamma_k.segment(k,m_z);       // Recover gamma_segment
+    SparseMatrix<Type> S_block = S.block(k,k,m_z,m_z);  // Recover S_i
+    nll -= Type(0.5)*m_z*log_lambda(z) - 0.5*lambda(z)*GMRF(S_block).Quadform(gamma_segment);
+    k += m_z;
   }
 
   // Linear predictor
-  //vector<Type> mu = X*alpha + Z*gamma + A_is*omega/exp(log_tau);
-  vector<Type> mu = X*alpha + Z*gamma;
-  vector<Type> devresid( Y.size() );
-  // Sparse product of matrix A and 3D array epsilon_stc
-  for( int row=0; row<Aistc.rows(); row++ ){
-    if( (!isNA(Aistc(row,2))) & (!isNA(Aistc(row,3))) ){   // Ignoring epsilon_stc when `sem` not passed to fit(.)
-      mu(Aistc(row,0)) += Ax(row) * epsilon_stc(Aistc(row,1),Aistc(row,2),Aistc(row,3)) / exp(log_tau);
+  vector<Type> p_i = X_ij*alpha_j + Z_ik*gamma_k;
+  p_i += multiply_3d_sparse( Aistc_zz, Axi_z, epsilon_stc, p_i.size() ) / exp(log_tau);
+  for( int i=0; i<p_i.size(); i++ ){
+    if( (n_h>0) ){     // (!isNA(c_i(i))) & (!isNA(t_i(i))) &
+      h = c_i(i)*n_t + t_i(i);
+      p_i(i) -= delta_h(h);
     }
   }
-  for(int i=0; i<Y.size(); i++){
-    if( (n_k>0) ){     // (!isNA(c_i(i))) & (!isNA(t_i(i))) &
-      k = c_i(i)*n_t + t_i(i);
-      mu(i) -= delta_k(k);
-    }
-    nll -= dnorm(Y(i), mu(i), sigma, true);
-    devresid(i) = Y(i) - mu(i);
+  // Likelihood
+  vector<Type> devresid_i( y_i.size() );
+  for( int i=0; i<y_i.size(); i++ ){
+    nll -= dnorm( y_i(i), p_i(i), exp(log_sigma(0)), true );
+    devresid_i(i) = y_i(i) - p_i(i);
   }
 
   // Predictions
-  vector<Type> mu_pred = predX*alpha + predZ*gamma;
-  // Sparse product of matrix A and 3D array epsilon_stc
-  for( int row=0; row<predAistc.rows(); row++ ){
-    if( (!isNA(predAistc(row,2))) & (!isNA(predAistc(row,3))) ){   // Ignoring epsilon_stc when `sem` not passed to fit(.)
-      mu_pred(predAistc(row,0)) += predAx(row) * epsilon_stc(predAistc(row,1),predAistc(row,2),predAistc(row,3)) / exp(log_tau);
-    }
-  }
-  for( int row=0; row<predAistc.rows(); row++ ){
-    if( (n_k>0) ){     // (!isNA(c_i(i))) & (!isNA(t_i(i))) &
-      k = predAistc(row,3)*n_t + predAistc(row,2);
-      mu_pred(predAistc(row,0)) -= delta_k(k);
+  vector<Type> p_g = X_gj*alpha_j + Z_gk*gamma_k;
+  p_g += multiply_3d_sparse( Agstc_zz, Axg_z, epsilon_stc, p_g.size() ) / exp(log_tau);
+  for( int g=0; g<p_g.size(); g++ ){
+    if( (n_h>0) ){     // (!isNA(c_i(i))) & (!isNA(t_i(i))) &
+      h = c_g(g)*n_t + t_g(g);
+      p_g(g) -= delta_h(h);
     }
   }
 
   // Reporting
   Type range = pow(8.0, 0.5) / exp( log_kappa );
   REPORT( range );
-  ADREPORT(alpha);
-  REPORT( mu );
-  REPORT( mu_pred );
-  REPORT( Q_kk );
-  REPORT( Q_spatial );
+  REPORT( p_i );
+  REPORT( p_g );
+  REPORT( Q_hh );
+  REPORT( Q_ss );
   //REPORT( Q_joint );
-  REPORT( devresid );
+  REPORT( devresid_i );
 
   return nll;
 }
