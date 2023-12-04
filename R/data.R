33c8aa09fe934e097bb890a4f666ca7365a83c6a
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2023-12-04 11:22:16 -0800

    adding SAR igraph example
    Former-commit-id: a0e397ae0344a51827379660f4db46e0ce63b029 [formerly 6ba2beb809b38bec50f1b80cd03fb6c410303e88] [formerly 60cdd907cb130ceb6dcea381aad54659893b9010 [formerly fd87750d88d021bfeb95b83e9c24253ed37b18fd]]
    Former-commit-id: 0b6044f78e91261635b56e5e629957b60988c615 [formerly d0da802522a961814ec4e768b92c6ded370bc90c]
    Former-commit-id: 6b2b3f4b40838cdc9bbe16df470f8f97501ad1a4

--- R/data.R
+++ R/data.R
@@ -10,4 +10,14 @@
 #' @keywords data
 NULL
 
-
+#' North Pacific salmon returns
+#'
+#' Data used to demonstrate and test multivariate second-order autoregressive
+#' models using a simultaneous autoregressive (SAR) process across regions.
+#' Data are from \doi{10.1002/mcf2.10023}
+#'
+#' @name salmon_returns
+#' @docType data
+#' @usage data(salmon_returns)
+#' @keywords data
+NULL
