40876bbef6b701730d94a25b937ad2c3abcb8107
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2023-12-11 16:01:08 -0800

    adding integrated-test and example to fit(.)
    Former-commit-id: c0bd97dae2ee22a9e14c58693d0b3090adc81fcb [formerly d578065ed8ed53d61f0e32b9c57a1062e43824bd] [formerly 93d3e47bc418c4e172b2aff4d84459a80ad692cd [formerly 0a64741287cc36c0dbfa8905ef9ecf09741d77f5]]
    Former-commit-id: fd546603521425409a0d467ced103e4c937ef12c [formerly c6654bd96ae9ae177439329aa5aac76578fe207a]
    Former-commit-id: 5997bdd0d414c55ede6abd4aacfb3f08df6e5f27

---
+++ tests/testthat.R
@@ -0,0 +1,10 @@
+
+
+library(testthat)
+library(tinyVAST)
+
+# Run tests
+testthat::test_check("tinyVAST")
+
+# Run from local directory
+# testthat::test_dir( "C:/Users/James.Thorson/Desktop/Git/dsem/tests/testthat/", reporter="check" )
