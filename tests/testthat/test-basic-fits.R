b892d59cdb043eaaf139ffca6b32e331e2a3622e
Jim Thorson <James.T.Thorson@gmail.com>
2025-02-27 12:25:05 -0800

    fix time_term test

--- tests/testthat/test-basic-fits.R
+++ tests/testthat/test-basic-fits.R
@@ -95,8 +95,7 @@ test_that("tinyVAST works as dsem", {
                    data = data,
                    times = isle_royale[,1],
                    variables = colnames(isle_royale[,2:3]),
-                   formula = logn ~ 0 + var,
-                   control = tinyVASTcontrol(run_model=FALSE) )
+                   formula = logn ~ 0 + var )
 
   expect_equal( as.numeric(fit1$opt$obj), as.numeric(fit2$opt$obj), tol = 1e-3  )
   expect_equal( as.numeric(fit1$opt$obj), 5.781919, tol = 1e-3 )
