cd57b1e529539a75ec3cec16bf4cc29034034dbf
Jim Thorson <James.T.Thorson@gmail.com>
2025-03-15 12:15:06 -0700

    trying to turn off all tests for exploring valgrind

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,7 +4,7 @@ library(testthat)
 library(tinyVAST)
 
 # Run tests
-testthat::test_check("tinyVAST")
+#testthat::test_check("tinyVAST")
 
 # Run from local directory
 # testthat::test_dir( "C:/Users/James.Thorson/Desktop/Git/tinyVAST/tests/testthat/", reporter="check" )
