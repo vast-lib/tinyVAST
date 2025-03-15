259fb3436d5dcfdc0b55ec29f54ab153374d288c
Jim Thorson <James.T.Thorson@gmail.com>
2025-03-15 08:08:21 -0700

    Create setup_rhub.R

---
+++ scratch/setup_rhub.R
@@ -0,0 +1,21 @@
+
+# Directions from: https://r-hub.github.io/rhub/articles/rhubv2.html
+setwd( R'(C:\Users\James.Thorson\Desktop\Git\tinyVAST)' )
+
+library(rhub)
+
+# Options
+rhub_platforms()
+
+# Setup
+rhub_setup(overwrite = FALSE)
+
+# Check
+rhub::rhub_doctor()
+
+# Run
+platforms = c( "m1-san", "clang-ubsan", "valgrind" )
+# 2 -- UBSAN
+# 9 -- clang-ubsan
+# 30 -- valgrind
+rhub::rhub_check( platforms = platforms )
