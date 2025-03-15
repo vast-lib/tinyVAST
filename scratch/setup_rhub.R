c28743f7a15386236db7ea11f49d05d12c480afb
Jim Thorson <James.T.Thorson@gmail.com>
2025-03-15 09:13:20 -0700

    Update setup_rhub.R

--- scratch/setup_rhub.R
+++ scratch/setup_rhub.R
@@ -24,3 +24,10 @@ rhub::rhub_check( platforms = platforms )
 if( FALSE ){
   rhub::rhub_check( platforms = "valgrind", branch = "dev" )
 }
+
+# HOW TO CHECK `valgrind` FOR ISSUES
+# Click Actions tab -> click R-hub tab on left-hand side ->
+# click action -> job -> "Run r-hub/actions/run-check@v1"
+# SEARCH: "Artifact download URL" to find URL to download errors
+# e.g., here: https://github.com/vast-lib/tinyVAST/actions/runs/13868485634/job/38811880343
+# -> Check tinyVAST-Ex.Rout and search "tinyVAST.cpp" for errors
