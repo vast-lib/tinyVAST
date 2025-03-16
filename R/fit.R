6a3378f9b3d91b83bbe4541f7c4ab25f8e5c94d2
Jim Thorson <James.T.Thorson@gmail.com>
2025-03-15 21:29:19 -0700

    re-add one example

--- R/fit.R
+++ R/fit.R
@@ -136,6 +136,31 @@
 #' \item{deviance_explained}{output from \code{\link{deviance_explained}}}
 #' }
 #'
+#' @examples
+#' # Simulate a seperable two-dimensional AR1 spatial process
+#' n_x = n_y = 25
+#' n_w = 10
+#' R_xx = exp(-0.4 * abs(outer(1:n_x, 1:n_x, FUN="-")) )
+#' R_yy = exp(-0.4 * abs(outer(1:n_y, 1:n_y, FUN="-")) )
+#' z = mvtnorm::rmvnorm(1, sigma=kronecker(R_xx,R_yy) )
+#'
+#' # Simulate nuissance parameter z from oscillatory (day-night) process
+#' w = sample(1:n_w, replace=TRUE, size=length(z))
+#' Data = data.frame( expand.grid(x=1:n_x, y=1:n_y), w=w, z=as.vector(z) + cos(w/n_w*2*pi))
+#' Data$n = Data$z + rnorm(nrow(Data), sd=1)
+#'
+#' # Add columns for multivariate and/or temporal dimensions
+#' Data$var = "n"
+#'
+#' # make SPDE mesh for spatial term
+#' mesh = fmesher::fm_mesh_2d( Data[,c('x','y')], n=100 )
+#'
+#' # fit model with cyclic confounder as GAM term
+#' out = tinyVAST( data = Data,
+#'                 formula = n ~ s(w),
+#'                 spatial_domain = mesh,
+#'                 space_term = "n <-> n, sd_n" )
+#'
 #' @useDynLib tinyVAST, .registration = TRUE
 #' @export
 tinyVAST <-
