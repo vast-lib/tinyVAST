f24021535296993aa5fd64bbe14362db5fe8dc54
Jim Thorson <James.T.Thorson@gmail.com>
2025-03-15 21:24:51 -0700

    Create run_linux.R

---
+++ scratch/run_linux.R
@@ -0,0 +1,14 @@
+
+# Start -> Search "cmd"
+# To install R in command-line:
+#  1.  "wsl --install"
+#  2.  "wsl sudo apt update"
+#  3.  "wsl sudo apt install r-base"
+#  4.  "wsl sudo apt install cmake"        # CPP compiles
+#  5.  "wsl sudo apt install libssl-dev"      # Unknown
+#  6.  "wsl sudo apt-get install gdal-bin"     # GDAL for spatial stuff
+#  7.  "wsl sudo apt-get install libgdal-dev"  # ditto
+#  8.  "wsl sudo apt install valgrind"   # valgrind
+#  9.  "wsl git clone https://github.com/vast-lib/tinyVAST"
+# To open R in command-line:  "wsl R"
+# Then use R as normal
