0dc33356d1b64992142ad34577de640f3081c276
Jim Thorson <James.T.Thorson@gmail.com>
2025-09-08 09:51:22 -0700

    add data for forecasting vignette

--- R/data.R
+++ R/data.R
@@ -107,3 +107,18 @@ NULL
 #' @keywords data
 NULL
 
+#' Data to demonstrate probabilistic forecasting
+#'
+#' Data to estimate probabilistic forecasting, which bridges uncertainty
+#' between short-term (decadal) projections and long-term (end-of-century)
+#' forecasts.  Here we use capelin sampling from a survey trawl survey
+#' in the eastern and northern Bering Sea, as well as annual
+#' sea surface temperature anomalies.  The data set includes data frames
+#' for both the fitted data \code{Data} and the projected values \code{New_Data}
+#'
+#' @name bering_sea_capelin_forecasts
+#' @docType data
+#' @usage data(bering_sea_capelin_forecasts)
+#' @keywords data
+NULL
+
