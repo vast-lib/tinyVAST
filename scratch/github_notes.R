a48d631abfc75941938793fba443dc39c24303fb
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2026-04-14 12:45:00 -0700

    Create github_notes.R

---
+++ scratch/github_notes.R
@@ -0,0 +1,11 @@
+
+
+# To fix an error in github commits
+#  * Type "cmd" in Windows search-bar to open Windows powershell
+#  * Change directory to location for github e.g., type "cd C:\Users\James.Thorson\Desktop\Git\tinyVAST"
+#  * Find commit immediately prior to bad commit and click "Copy full SHA" to get SHA hash
+#  * In powershell, type "git reset --hard [SHA hash]"
+#  * Open Windows Explorer and browse local copy to confirm that it reverted to correct state
+#  * Open Github for Windows and confirm that there are "Fetch" updates, but do not fetch (fetching these would revert to current commit for branch)
+#  * In powershell, type "git push origin fix_bug --force" to update GitHub cloud back to that local state
+
